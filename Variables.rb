508cb75740fbf19381615ea6ee172b1bdfcb89b2
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2022-01-17 21:40:39 +0700

    Create Variables.rb

---
+++ Variables.rb
@@ -0,0 +1,7 @@
+character_name = "John"
+character_age = "35"
+puts ("There once was a man named " + character_name)
+puts ("he was" + character_age + "years old.")
+character_name = "Bill"
+puts ("He really liked the name " + character_name)
+puts ("but didn't like being " + character_age)
