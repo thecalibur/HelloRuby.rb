893b321a7773b48e4f5f902e65cbf6a45a54eb9d
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2022-01-21 18:37:48 +0700

    Create PrintingTheOutphut.rb

---
+++ PrintingTheOutphut.rb
@@ -0,0 +1,9 @@
+print "What is your first name?"
+first_name = gets.chomp
+print "What is your last name?"
+last_name = gets.chomp
+print "Which city do you live?"
+city = gets.chomp
+print "Which state do you live?"
+state = gets.chomp
+puts "your name is #{first_name} #{last_name} and you live in #{city} #{state}"
