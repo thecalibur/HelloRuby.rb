b23e65e4763b56aefd837cfe61cae1eddd5d2879
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2022-01-21 20:27:32 +0700

    Create And.rb

---
+++ And.rb
@@ -0,0 +1,8 @@
+# boolean_1 = 77 < 78 && 77 < 77
+boolean_1 = true && false
+
+# boolean_2 = true && 100 >= 100
+boolean_2 = true && true
+
+# boolean_3 = 2**3 == 8 && 3**2 == 9
+boolean_3 = true && true
