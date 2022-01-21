2deee1a0c5737f21a53b479674c2744368c49cc4
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2022-01-21 18:57:03 +0700

    Create FormattingWithStringMethods.rb
    capitalize sẽ làm cho chữ cái đầu của chuỗi được viết hoa

---
+++ FormattingWithStringMethods.rb
@@ -0,0 +1,15 @@
+print "What is your first name?"
+first_name = gets.chomp
+first_name.capitalize!
+print "What is your last name?"
+last_name = gets.chomp
+last_name.capitalize!
+print "Which city do you live?"
+city = gets.chomp
+city.capitalize!
+print "Which state do you live?0"
+state = gets.chomp
+state.upcase!
+puts "your name is #{first_name} #{last_name} and you live in #{city} #{state}"
+
+
