0d597cab947e992a8a8ad3e7978e552fa914b14e
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2022-01-22 21:10:54 +0700

    Create IfElseAndElsif.rb

---
+++ IfElseAndElsif.rb
@@ -0,0 +1,11 @@
+print "Enter number a: "
+a = gets.chomp
+print "Enter number b: "
+b = gets.chomp
+if a < b
+   print "a is less than b!"
+elsif b < a
+  print "b is less than a!"
+else
+  print "b is equal to a!"
+end
