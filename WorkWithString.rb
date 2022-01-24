bce8230411e8f73c64f7f8be8f92ca8f47f605e7
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2022-01-24 11:06:05 +0700

    Create WorkWithString.rb

---
+++ WorkWithString.rb
@@ -0,0 +1,11 @@
+print "Pleathe enter a thtring: " 
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "s"
+  user_input.gsub!(/s/, "th")
+else
+  puts "There are no 's's in your string."
+end
+
+puts "Your new thtring is #{user_input}."
