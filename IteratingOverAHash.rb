4ccc06347f241fa80a5afd93a37a7695d292de36
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2022-02-17 19:43:31 +0700

    Create IteratingOverAHash.rb

---
+++ IteratingOverAHash.rb
@@ -0,0 +1,11 @@
+lunch_order = {
+  "Ryan" => "wonton soup",
+  "Eric" => "hamburger",
+  "Jimmy" => "sandwich",
+  "Sasha" => "salad",
+  "Cole" => "taco"
+}
+
+lunch_order.each do |person, order| 
+  puts order
+end
