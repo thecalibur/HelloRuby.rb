b1bab5f2dad991eab745fc4c1bbe196988a4c1b9
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2022-03-02 20:04:35 +0700

    Create CreateYourOwn.rb

---
+++ CreateYourOwn.rb
@@ -0,0 +1,12 @@
+# Define your method below!
+
+def greeting
+  puts "Hello, Mr Bill!"
+end
+
+
+
+# Define your method above this line.
+
+greeting # Ignore this for now. We'll explain
+         # it in the next exercise!
